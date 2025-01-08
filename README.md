bf487739eaeb0651ccb358b65295bdc9043f0541
eniar <isbi@iki.fi>
2025-01-08 12:09:27 +0200

    Functional version.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# stuff
+
+Multiples of A and B
+
+Command to run: python Multiples.py <input_file> <output_file>
+
+input.txt is provided.
+
+Code is written to handle excess whitespace. Task description didn't specify how to handle duplicate lines in input file. Implementation ignores the duplicates and genarates since result line. 
---
+++ multiples/Multiples.py
@@ -0,0 +1,105 @@
+# pylint: disable=invalid-name
+# pylint: enable=invalid-name
+"""
+Solution for task of Multiples of A and B.
+"""
+import argparse
+
+
+class Multiples():
+
+    """
+    Class to calculate multiples smaller than given value.
+    """
+
+    def __init__(self):
+        """
+        Initialize object with command line arguments.
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument("input_file", help="Path to input file.")
+        parser.add_argument("output_file", help="Path to output file.")
+        self.args = parser.parse_args()
+
+    def read_input_file(self):
+        """
+        Function to open and read file contents.
+        
+        Returns:
+            list: Input values.
+        """
+        incoming_list_of_strings = []
+        with open(self.args.input_file, "r", encoding="utf-8") as incoming:
+            for line in incoming:
+                if line != "\n":
+                    incoming_list_of_strings.append(line.strip().split())
+        return incoming_list_of_strings
+
+    def multiply(self, incoming_values:list):
+        """
+        The multiplication as specified.
+        
+        Inputs:
+            list: Values read from the input file.
+        Returns:
+            dict: Calculation results identified by the limiting third value.
+        """
+        outgoing_dict = {}
+        for alist in incoming_values:
+            multiplier = 1
+            temp_list = []
+            while True:
+                result1 = int(alist[0]) * multiplier
+                result2 = int(alist[1]) * multiplier
+                if result1 < int(alist[2]):
+                    temp_list.append(result1)
+                if result2 < int(alist[2]):
+                    temp_list.append(result2)
+                multiplier += 1
+                if result1 > int(alist[2]) and result2 > int(alist[2]):
+                    break
+            outgoing_dict.update({alist[2]:temp_list})
+        return outgoing_dict
+
+    def format_output(self, outgoing:dict):
+        """
+        Modify the string into a given format.
+
+        Inputs:
+            dict: Calculation results.
+        Returns:
+            str: Calculation result sorted and in printable form.
+        """
+        sorting_values = sorted(outgoing, key=lambda key: len(outgoing[key]))
+        printable_file = ""
+        for value in sorting_values:
+            printable_line = f"{value}:"
+            for item in outgoing[value]:
+                printable_line += f"{item} "
+            printable_file += printable_line + "\n"
+        return printable_file
+
+    def write_output_file(self, content:str):
+        """
+        Write results in outputfile and on screen.
+
+        Inputs:
+            str: Ready to print text.
+        """
+        with open(self.args.output_file,"w", encoding="utf-8") as outgoing:
+            outgoing.write(content)
+
+    def run(self):
+        """
+        Runner code to make the module work.
+        """
+        multiples_of_a_and_b = Multiples()
+        file_content = multiples_of_a_and_b.read_input_file()
+        results = multiples_of_a_and_b.multiply(file_content)
+        ready_to_print = multiples_of_a_and_b.format_output(results)
+        multiples_of_a_and_b.write_output_file(ready_to_print)
+        print(ready_to_print)
+
+if __name__ == "__main__":
+    run_forest = Multiples()
+    run_forest.run()
